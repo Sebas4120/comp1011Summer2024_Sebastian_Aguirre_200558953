49082e50a9654612475ef436e59ac0f2ecbf0443
Sebas4120 <144409264+Sebas4120@users.noreply.github.com>
2024-05-14 20:50:00 -0400

    Martes 05/14-08:49pm

--- src/main/java/ca/georgiancollege/comp1011summer2024_sebastian_aguirre_200558953/Camera.java
+++ src/main/java/ca/georgiancollege/comp1011summer2024_sebastian_aguirre_200558953/Camera.java
@@ -5,7 +5,7 @@ public class Camera {
     private String lens=""; // Estos cuando los declaras asi nunca seran nulos
     private String sensor="";
 
-    private String color = "red";
+    private String color = "";
     private String make="";
 
     private String model="";
@@ -21,17 +21,70 @@ public class Camera {
 
     }
     public Camera(String lens, String sensor, String color, String make, String model) {
+//        this.lens = lens;
+//        this.sensor = sensor;
+//        this.color = color;
+//        this.make = make;
+//        this.model = model;
+        setModel(model);
+        setMake(make);
+        setColor(color);
+        setSensor(sensor);
+        setLens(lens);
+    }
+
+    private void validate(String title, String data){
+        if (data.trim().isEmpty()){
+            throw new IllegalArgumentException(title + "is too short");
+        }
+    }
+    public String getLens() {
+        return lens;
+    }
+
+    public void setLens(String lens) {
+        validate("Lens", lens);
         this.lens = lens;
+    }
+
+    public String getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(String sensor) {
+        validate("Sensor", sensor);
         this.sensor = sensor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        validate ("color", color);
         this.color = color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        validate("make", make);
         this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        validate("model", model);
         this.model = model;
     }
 
-
-
     public boolean takePicture (int howMany){
-        numOfPhotosTaken+= howMany;
+        numOfPhotosTaken+=howMany;
         return true;
     }
 
--- src/main/java/ca/georgiancollege/comp1011summer2024_sebastian_aguirre_200558953/CameraController.java
+++ src/main/java/ca/georgiancollege/comp1011summer2024_sebastian_aguirre_200558953/CameraController.java
@@ -1,4 +1,36 @@
 package ca.georgiancollege.comp1011summer2024_sebastian_aguirre_200558953;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CameraController {
+
+    @FXML
+    private TextField color,lens,make,model,sensor;
+
+    @FXML
+    private Label error,output;
+
+
+    @FXML
+    void onSubmit(ActionEvent event) {
+        model.getText();//gets the text inputted by the user in the TextField
+        error.getText();
+        error.setText("Wrong input!");
+
+
+
+        /*
+            Add the code to the controller
+                evaluated each Textfield
+                    determine that each TextField has a length() of at least 1
+                        if not, send an error message to the error label
+                        if so, instantiate a new Camera object and pass the values of TextFields
+         */
+        Camera camera = new Camera(model.getText(), make.getText(), color.getText(),
+                sensor.getText(), lens.getText());
+    }
+
 }
